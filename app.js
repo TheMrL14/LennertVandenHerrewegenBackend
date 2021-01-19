51d5edcaddcac7a924a8076ca99519daa62ba296
TheMrL14 <lennert.vanden.herrewegen@student.ehb.be>
2021-01-19 12:57:45 +0100

    db init

--- app.js
+++ app.js
@@ -49,11 +49,10 @@ app.use(function (err, req, res, next) {
 });
 
 var connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_SECRET,
+  database: process.env.MYSQL_DB,
 });
 
 connection.connect(function (err) {
