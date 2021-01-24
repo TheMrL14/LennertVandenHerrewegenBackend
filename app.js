8267295f031de01fa708c5f3dc8f424566daf3c9
TheMrL14 <lennert.vanden.herrewegen@student.ehb.be>
2021-01-24 16:52:23 +0100

    quick bug fix

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 
 const corsOptions = {
+  //origin: "http://localhost:9000",
   origin: "https://www.lennertvh.xyz",
   methods: "GET,HEAD,POST,OPTIONS,DELETE",
   allowedHeaders: "",
